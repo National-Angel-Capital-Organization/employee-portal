5835d4c6283a64e41e949b82485bcd0838529d25
Bryan Hunter <bdhunter3141@gmail.com>
2018-10-29 14:55:36 -0400

    setup cookies with placeholders.

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,22 @@
 import Head from 'next/head'
 import Navbar from './navbar'
 
+const middleWare = (props) => {
+  return (<div>
+  <Navbar />
+    <div className="container">
+      {props.children}
+    </div>
+  </div>)
+}
+
 const Layout = (props) => (
   <div>
     <Head>
       <title>NACO Employee Portal</title>
       <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css" />
     </Head>
-    <Navbar />
-    <div className="container">
-    {props.children}
-    </div>
+    {middleWare(props)}
   </div>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from '../components/layout'
+import nookies from 'nookies'
 
-const Index = () => (
+const Index = (props) => (
   <Layout>
     <div>
       <h1>NACO Employee Portal</h1>
@@ -8,5 +9,17 @@ const Index = () => (
   </Layout>
 )
 
+Index.getInitialProps = async (ctx) => {
+  await nookies.set(ctx, 'token', '123456')
+
+  let cookies = await nookies.get(ctx)
+  if(cookies.token) {
+  console.log('hello')
+  }
+  return {
+    hello: 'world',
+    cookies: cookies
+  }
+}
 
 export default Index
